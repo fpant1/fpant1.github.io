2633668081abedc787aa3e4f56a60de98c69bee6
fpant1 <119616118+fpant1@users.noreply.github.com>
2025-11-05 15:56:05 +0000

    Deploying to gh-pages from @ fpant1/fpant1.github.io@b73117220a8c24bd8065aa3d7ab6cf11146b3956 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-optimising-robotic-search-algorithms",
           title: 'Optimising Robotic Search Algorithms',
-          description: "A study exploring how speed, accuracy, and thoroughness interact in robotic search systems, using the Pololu 3Pi+ platform. The project investigates search pattern parameters to enhance autonomous navigation for applications such as disaster response, industrial automation, and environmental mapping.",
+          description: "Balancing Speed, Accuracy, and Thoroughness in Autonomous Navigation Systems",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
